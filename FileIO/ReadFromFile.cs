553ff155e69ad89d7b36f06ee1d9805b967feeda
Szőcs Gábor <szocsgabor@turr.hu>
2026-01-04 12:05:02 +0100

    Add project files.

---
+++ FileIO/ReadFromFile.cs
@@ -0,0 +1,45 @@
+﻿namespace FileIO
+{
+    public class ReadFromFile
+    {
+        public List<List<string>> FileRead(string fileName, int itemCounter, char splitter, bool header)
+        {
+            List<List<string>> result = new List<List<string>>();
+            try
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                if (header)
+                {
+                    lines = lines.Skip(1).ToArray();
+                }
+                foreach (string line in lines)
+                {
+                    string[] items = line.Split(splitter);
+
+                    if (items.Length == itemCounter)
+                    {
+                        result.Add(new List<string>(items));
+                    }
+                    else
+                    {
+                        Console.WriteLine("A sor nem " +
+                            "tartalmaz megfelelő szamú elemet");
+                    }
+                }
+            }
+            catch (FileNotFoundException error1)
+            {
+                Console.WriteLine("A fájl nem található: (error1)", error1);
+            }
+            catch (IOException error2)
+            {
+                Console.WriteLine("A fájl nem található: (error2)", error2);
+            }
+            catch (Exception error3)
+            {
+                Console.WriteLine("Váratlan hiba történt.: (error3)", error3);
+            }
+            return result;
+        }
+    }
+}
---
+++ FileIO/WriteToFile.cs
@@ -0,0 +1,10 @@
+﻿namespace FileIO
+{
+    public class WriteToFile
+    {
+        public void FileWrite(string fileName, List<string> data)
+        {
+            File.WriteAllLines(fileName, data);
+        }
+    }
+}
