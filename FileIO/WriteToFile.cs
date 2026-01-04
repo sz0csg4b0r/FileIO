a3726daa605405aefdd3af5ba47aac2f365fac28
sz0csg4b0r <szocsgabor@turr.hu>
2026-01-04 12:11:44 +0100

    tesztAdd README.md for project documentation

--- FileIO/WriteToFile.cs
+++ FileIO/WriteToFile.cs
@@ -5,6 +5,7 @@
         public void FileWrite(string fileName, List<string> data)
         {
             File.WriteAllLines(fileName, data);
+            //Ez csak egy teszt sor.
         }
     }
 }
